75afbfd96fa60cc069eb6ea1840f8033f839f3d3
Stathis Karaimitriou <stathis.karadimitriou@gmail.com>
2020-06-16 17:47:19 +0300

    RecyclerView

--- app/src/main/java/com/stathis/elmepaunivapp/Professors.java
+++ app/src/main/java/com/stathis/elmepaunivapp/Professors.java
@@ -1,14 +1,25 @@
 package com.stathis.elmepaunivapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.BaseAdapter;
 
 public class Professors extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_professors);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        recyclerView = findViewById(R.id.recyclerView);
     }
 }
