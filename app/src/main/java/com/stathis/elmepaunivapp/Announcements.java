e3e241bf530e6cdfc942f606e924f1a2d1559bd4
Stathis Karaimitriou <stathis.karadimitriou@gmail.com>
2020-06-16 17:18:19 +0300

    WebView

--- app/src/main/java/com/stathis/elmepaunivapp/Announcements.java
+++ app/src/main/java/com/stathis/elmepaunivapp/Announcements.java
@@ -1,14 +1,51 @@
 package com.stathis.elmepaunivapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.webkit.WebView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Announcements extends AppCompatActivity {
+
+    private WebView webview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_announcements);
+        //load webview with school web content
+        WebView webview = findViewById(R.id.web_announcements);
+        webview.loadUrl("https://mst.hmu.gr/news_gr/");
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setSelectedItemId(R.id.nav_home);
+        //listener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Intent i;
+                switch(item.getItemId()){
+                    case R.id.nav_home:
+                        i = new Intent(Announcements.this,Dashboard.class);
+                        startActivity(i);
+                        return true;
+                    case R.id.nav_one:
+                    case R.id.nav_two:
+                    case R.id.nav_three:
+                }
+                return false;
+            }
+        });
+
     }
 }
