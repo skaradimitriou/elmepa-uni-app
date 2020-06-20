a135b0ed5e1fa5a9bee4583bf8a88ccc7e98b968
Stathis Karaimitriou <stathis.karadimitriou@gmail.com>
2020-06-20 18:33:51 +0300

    Students Screen general fixes

--- app/src/main/java/com/stathis/elmepaunivapp/Students.java
+++ app/src/main/java/com/stathis/elmepaunivapp/Students.java
@@ -1,14 +1,85 @@
 package com.stathis.elmepaunivapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.stathis.elmepaunivapp.models.DeptFieldsOfStudy;
+import com.stathis.elmepaunivapp.models.Programmes;
+import com.stathis.elmepaunivapp.recyclerview.FieldsAdapter;
+import com.stathis.elmepaunivapp.recyclerview.ItemClickListener;
+
+import java.util.ArrayList;
 
 public class Students extends AppCompatActivity {
+
+    private RecyclerView fields_recView;
+    private FieldsAdapter fieldsAdapter;
+    private ArrayList<DeptFieldsOfStudy> fieldsOfStudy;
+    private Button btn;
+    private CardView schedule;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_students);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        //createLists
+        createLists();
+
+        //rec Views & adapters
+        fields_recView = findViewById(R.id.diff_directions);
+        fieldsAdapter = new FieldsAdapter(fieldsOfStudy, new ItemClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+
+            //do nothing
+            @Override
+            public void onItemClick(DeptFieldsOfStudy item) {
+                //do nothing
+            }
+
+            @Override
+            public void onProgrammesClick(Programmes programmes) {
+                //do nothing
+            }
+        });
+        fields_recView.setAdapter(fieldsAdapter);
+
+        schedule = findViewById(R.id.schedule_cardview);
+        schedule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //click action
+            }
+        });
+
+        btn = findViewById(R.id.schedule_card_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //click action
+            }
+        });
+
+    }
+
+    private void createLists() {
+        fieldsOfStudy = new ArrayList<>();
+        fieldsOfStudy.add(new DeptFieldsOfStudy("Επιστήμη των Δεδομένων & Τεχνολογίες Πληροφορικής", R.drawable.data));
+        fieldsOfStudy.add(new DeptFieldsOfStudy("Διοίκηση Επιχειρήσεων & Οργανισμών", R.drawable.business));
+        fieldsOfStudy.add(new DeptFieldsOfStudy("Ψηφιακό Μάρκετινγκ και Επικοινωνία", R.drawable.digitalmkt));
     }
 }
