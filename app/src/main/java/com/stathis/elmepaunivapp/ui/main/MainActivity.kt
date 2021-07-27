7cac509398d4311d2f97c97056cbc54bd759c538
stathiskaradimitriou <stathis.karadimitriou@gmail.com>
2021-07-28 00:09:57 +0300

    Open Professors Search on Widget Tap

--- app/src/main/java/com/stathis/elmepaunivapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/stathis/elmepaunivapp/ui/main/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.stathis.elmepaunivapp.ui.main
 
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -8,24 +9,25 @@ import com.stathis.elmepaunivapp.abstraction.ElmepaActivity
 
 class MainActivity : ElmepaActivity(R.layout.activity_main) {
 
+    private lateinit var navView : BottomNavigationView
+    private lateinit var navController : NavController
+
+
     override fun init() {
-        val navView: BottomNavigationView = findViewById(R.id.bottom_navigation_menu)
-        val navController = findNavController(R.id.nav_host_fragment)
-        navView.setupWithNavController(navController)
+        navView = findViewById(R.id.bottom_navigation_menu)
+        navController = findNavController(R.id.nav_host_fragment)
     }
 
     override fun startOps() {
-        val test = intent.getBooleanExtra("OPEN_PROFESSORS",false)
+        val cameFromWidget = intent.getBooleanExtra("OPEN_PROFESSORS",false)
 
-        when(test){
-            true -> loadProfessorsFragment()
+        when(cameFromWidget){
+            true -> navController.navigate(R.id.nav_search)
             else -> Unit
         }
+
+        navView.setupWithNavController(navController)
     }
 
     override fun stopOps() {}
-
-    private fun loadProfessorsFragment(){
-        val directions = findNavController(R.id.nav_host_fragment).navigate(R.id.nav_search)
-    }
 }
