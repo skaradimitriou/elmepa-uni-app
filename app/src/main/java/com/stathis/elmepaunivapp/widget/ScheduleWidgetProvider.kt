61754b084a234593311c6954b82af107a18e0e0f
stathiskaradimitriou <stathis.karadimitriou@gmail.com>
2021-07-29 23:39:54 +0300

    Schedule Widget Added

---
+++ app/src/main/java/com/stathis/elmepaunivapp/widget/ScheduleWidgetProvider.kt
@@ -0,0 +1,33 @@
+package com.stathis.elmepaunivapp.widget
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+import com.stathis.elmepaunivapp.R
+import com.stathis.elmepaunivapp.ui.main.MainActivity
+import com.stathis.elmepaunivapp.ui.webview.WebviewActivity
+
+class ScheduleWidgetProvider : AppWidgetProvider() {
+
+    override fun onUpdate(
+        context: Context?,
+        appWidgetManager: AppWidgetManager?,
+        appWidgetIds: IntArray?
+    ) {
+        if (appWidgetIds != null) {
+            for (appWidgetId in appWidgetIds) {
+                val intent = Intent(context, WebviewActivity::class.java)
+                    .putExtra("URL", "https://mst.hmu.gr/proptyxiako/orologio-programma-mathimaton/")
+                val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+
+                val views = RemoteViews(context?.packageName, R.layout.schedule_widget)
+                views.setOnClickPendingIntent(R.id.schedule_button, pendingIntent)
+
+                appWidgetManager?.updateAppWidget(appWidgetId, views)
+            }
+        }
+    }
+}
