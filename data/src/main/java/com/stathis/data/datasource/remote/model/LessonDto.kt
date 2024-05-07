a6fc0bec7396d1bde33c822b76a461d195322b47
Stathis Karadimitriou <stathis.karadimitriou@gmail.com>
2024-05-07 14:22:37 +0300

    Implementation: resolve syllabus issue where lessons appeared with false data

--- data/src/main/java/com/stathis/data/datasource/remote/model/LessonDto.kt
+++ data/src/main/java/com/stathis/data/datasource/remote/model/LessonDto.kt
@@ -5,6 +5,11 @@ data class LessonDto(
     val description: String? = null,
     val hours: String? = null,
     val mandatory: Boolean? = null,
-    val orientation: List<String>? = null,
+    val orientation: List<OrientationModelDto>? = null,
     val semester: String? = null
+)
+
+data class OrientationModelDto(
+    val mandatory: Boolean? = null,
+    val type: String? = null
 )
