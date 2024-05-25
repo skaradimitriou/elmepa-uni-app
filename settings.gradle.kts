e3e023d304e43d60b612f479f4be637d86f49f5e
Stathis Karadimitriou <stathis.karadimitriou@gmail.com>
2024-05-25 12:31:31 +0300

    Implementation: Decouple personnel into sub feature module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
-pluginManagement{
+pluginManagement {
     repositories {
         google {
-            content{
+            content {
                 includeGroupByRegex("com\\.android.*")
                 includeGroupByRegex("com\\.google.*")
                 includeGroupByRegex("androidx.*")
@@ -12,19 +12,20 @@ pluginManagement{
     }
 }
 
-dependencyResolutionManagement{
+dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories{
+    repositories {
         google()
         mavenCentral()
     }
 }
 
 rootProject.name = "ElmepaUniApp"
-include (":app")
+include(":app")
 include(":data")
 include(":domain")
 include(":core")
 include(":model")
 include(":feature")
 include(":feature:syllabus")
+include(":feature:personnel")
