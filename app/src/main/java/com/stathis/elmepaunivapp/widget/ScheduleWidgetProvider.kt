6c3ad4d05b361bf9b45c100853523fa14acdca72
Stathis Karadimitriou <stathis.karadimitriou@gmail.com>
2024-06-01 08:27:08 +0300

    Implementation: decouple common feature branch

--- app/src/main/java/com/stathis/elmepaunivapp/widget/ScheduleWidgetProvider.kt
+++ app/src/main/java/com/stathis/elmepaunivapp/widget/ScheduleWidgetProvider.kt
@@ -6,11 +6,11 @@ import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
 import android.widget.RemoteViews
+import com.stathis.common.webview.WebViewFragment
 import com.stathis.core.util.TITLE
 import com.stathis.core.util.URL
 import com.stathis.elmepaunivapp.R
 import com.stathis.elmepaunivapp.util.SCHEDULE_URL
-import com.stathis.feature.ui.webview.WebViewFragment
 
 class ScheduleWidgetProvider : AppWidgetProvider() {
 
