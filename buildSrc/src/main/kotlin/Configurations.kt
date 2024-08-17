6eff72028dc1a1abd7f34037d768c7c4ef496b5c
Stathis Karadimitriou <stathis.karadimitriou@gmail.com>
2024-08-17 23:44:19 +0300

    Update: Increase project version to 4.1.0

--- buildSrc/src/main/kotlin/Configurations.kt
+++ buildSrc/src/main/kotlin/Configurations.kt
@@ -13,8 +13,8 @@ object Configurations {
      */
 
     const val MAJOR_VERSION = 4
-    const val MINOR_VERSION = 0
-    const val PATCH_VERSION = 2
+    const val MINOR_VERSION = 1
+    const val PATCH_VERSION = 0
     const val VERSION_CODE = MAJOR_VERSION * 10000 + MINOR_VERSION * 100 + PATCH_VERSION
     const val VERSION_NAME = "$MAJOR_VERSION.$MINOR_VERSION.$PATCH_VERSION"
 }
