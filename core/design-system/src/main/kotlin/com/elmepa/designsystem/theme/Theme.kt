781b5ee817826b13197f2622bd4e81e9b43f96f0
Stathis Karadimitriou <64270931+skaradimitriou@users.noreply.github.com>
2025-03-02 12:05:07 +0200

    Impl: Add basic color palette

--- core/design-system/src/main/kotlin/com/elmepa/designsystem/theme/Theme.kt
+++ core/design-system/src/main/kotlin/com/elmepa/designsystem/theme/Theme.kt
@@ -1,51 +1,46 @@
 package com.elmepa.designsystem.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
+    primary = White,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    background = DarkModeSurface, //App background color,
+    onBackground = Color.White, //Text/icons on background
+    surface = DarkModeElement2, //UI surfaces like cards, sheets, dialogs
+    onSurface = Color.White, //Text/icons on surfaces
+    error = Color.Red, //Error messages, warning indicators
+    onError = Color.White, //wText/icons on error color
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    primary = Black,
     secondary = PurpleGrey40,
     tertiary = Pink40,
-    background = LessonOrange,
-    surface = LessonOrange,
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    background = GreyBg, //App background color,
+    onBackground = Color.Black, //Text/icons on background
+    surface = Color.White, //UI surfaces like cards, sheets, dialogs
+    onSurface = Color.Black, //Text/icons on surfaces
+    error = Color.Red, //Error messages, warning indicators
+    onError = Color.White, //wText/icons on error color
 )
 
 @Composable
 fun ElmepaAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+//        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+//            val context = LocalContext.current
+//            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+//        }
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
