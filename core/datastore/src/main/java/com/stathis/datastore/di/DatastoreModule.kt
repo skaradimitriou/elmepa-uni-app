a9184d06286cf9a58262e87995259e39524aab82
Stathis Karadimitriou <64270931+skaradimitriou@users.noreply.github.com>
2025-03-11 20:36:53 +0200

    Delete faq datastore old files

--- core/datastore/src/main/java/com/stathis/datastore/di/DatastoreModule.kt
+++ core/datastore/src/main/java/com/stathis/datastore/di/DatastoreModule.kt
@@ -1,11 +1,8 @@
 package com.stathis.datastore.di
 
 import android.app.Application
-import com.google.gson.Gson
 import com.stathis.datastore.caching.CacheManager
 import com.stathis.datastore.caching.CacheManagerImpl
-import com.stathis.datastore.datastore.FaqDataStore
-import com.stathis.datastore.datastore.FaqDataStoreImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -15,13 +12,6 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 class DatastoreModule {
-
-    @Provides
-    @Singleton
-    fun provideFaqDatastore(
-        app: Application,
-        gson: Gson
-    ): FaqDataStore = FaqDataStoreImpl(app, gson)
 
     @Provides
     @Singleton
