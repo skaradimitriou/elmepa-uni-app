7a5d7c2fd648e9c7fa649a6a0480728c7f69e459
Stathis Karadimitriou <64270931+skaradimitriou@users.noreply.github.com>
2025-03-24 16:22:29 +0200

    Clean up old personnel screen files

--- feature/personnel/src/main/java/com/stathis/personnel/util/PersonnelBindingAdapters.kt
+++ feature/personnel/src/main/java/com/stathis/personnel/util/PersonnelBindingAdapters.kt
@@ -1,33 +1,8 @@
 package com.stathis.personnel.util
 
-import android.view.View
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
-import com.bumptech.glide.Glide
-import com.stathis.common.R
 import com.stathis.model.common.LinkType
-
-@BindingAdapter("setPersonnelImage", "personnelGender")
-fun ImageView.setPersonnelImage(url: String, gender: String) {
-    val genderImg = when (gender) {
-        resources.getString(R.string.male) -> R.drawable.male
-        else -> R.drawable.female
-    }
-
-    Glide.with(context).load(url).placeholder(R.drawable.placeholder).error(genderImg).into(this)
-}
-
-@BindingAdapter("setPersonnelDescription")
-fun TextView.setPersonnelDescription(description: String) {
-    visibility = if (description.isEmpty()) {
-        View.GONE
-    } else {
-        View.VISIBLE
-    }
-
-    text = description
-}
 
 @BindingAdapter("setPersonnelProfession")
 fun TextView.setPersonnelProfession(description: String) {
