f1a0c0f2919764919e2a641f9083b79dded1de00
Stathis Karadimitriou <64270931+skaradimitriou@users.noreply.github.com>
2025-03-31 22:45:49 +0300

    Add new spacing tokens in theme

---
+++ core/design-system/src/main/kotlin/com/elmepa/designsystem/theme/Spacing.kt
@@ -0,0 +1,14 @@
+package com.elmepa.designsystem.theme
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+data class Spacing(
+    val xSmall: Dp = 4.dp,
+    val small: Dp = 8.dp,
+    val medium: Dp = 16.dp,
+    val large: Dp = 24.dp,
+    val xLarge: Dp = 32.dp,
+    val xxLarge: Dp = 48.dp,
+    val xxxLarge: Dp = 64.dp
+)
