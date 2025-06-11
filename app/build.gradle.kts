0fc53525f150ed94af435864cdd515c1fa6fbc60
Stathis Karadimitriou <64270931+skaradimitriou@users.noreply.github.com>
2025-06-11 22:25:45 +0300

    [CI] Create release workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,6 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            signingConfig = signingConfigs.getByName("debug")
         }
     }
 
