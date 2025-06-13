9e35c9eab43059b5a925e5e986f3a78ebb6dd858
Stathis Karadimitriou <64270931+skaradimitriou@users.noreply.github.com>
2025-06-13 23:07:52 +0300

    Create studentsv2 module

---
+++ feature/studentsv2/students-ui/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    alias(libs.plugins.elmepa.android.feature)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.elmepa.android.hilt)
+    alias(libs.plugins.elmepa.android.library)
+    alias(libs.plugins.elmepa.android.compose)
+    alias(libs.plugins.compose.compiler)
+}
+
+android {
+    namespace = "com.elmepa.students.ui"
+}
+
+dependencies {
+    implementation(project(":core:model"))
+    implementation(project(":core:common"))
+    implementation(project(":core:design-system"))
+
+    implementation(project(":core:domain"))
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+
+    implementation(libs.fragment.navigation)
+
+    implementation(libs.viewModelLifecycle)
+    implementation(libs.lifecycle.common)
+}
